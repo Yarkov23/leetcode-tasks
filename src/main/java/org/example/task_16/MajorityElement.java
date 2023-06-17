92347c657fadbf4a4c99d0e9e86c0e417dd25470
Yarkov23 <y.g.strukov@gmail.com>
2023-06-17 13:58:32 +0300

    Added majority element task solution

---
+++ src/main/java/org/example/task_16/MajorityElement.java
@@ -0,0 +1,31 @@
+package org.example.task_16;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        if (nums == null)
+            return 0;
+
+        if (nums.length == 1)
+            return nums[0];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = nums[0];
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+                if (map.get(num) > map.get(max)) {
+                    max = num;
+                }
+            } else {
+                map.put(num, 1);
+            }
+        }
+        return max;
+    }
+
+}
