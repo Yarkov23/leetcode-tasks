cf828f2c103420bf8ee6a39189f0d1656da45d43
Yarkov23 <y.g.strukov@gmail.com>
2023-06-22 15:42:27 +0300

    Added students by score sort task

---
+++ src/main/java/org/yarkov/medium/StudentsByScore.java
@@ -0,0 +1,15 @@
+package org.yarkov.medium;
+
+import java.util.Arrays;
+
+public class StudentsByScore {
+
+    // m - students, n - exams
+
+
+    public int[][] sortTheStudents(int[][] score, int k) {
+        Arrays.sort(score, (b, a) -> Integer.compare(a[k], b[k]));
+        return score;
+    }
+
+}
